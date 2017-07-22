f535661bdc3a6692738c2f703013072b23fa6558
Tsunenobu Kai <kai2nenobu@gmail.com>
2017-07-22 14:59:27 +0900

    Pollyで音声合成する

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -7,6 +7,7 @@ print('Loading function')
 
 s3 = boto3.client('s3')
 rekognition = boto3.client('rekognition')
+polly = boto3.client('polly')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
@@ -19,7 +20,12 @@ def lambda_handler(event, context):
     ## Rekognitionでラベルを検出する
     # TODO s3オブジェクトが画像じゃなかったらエラーにする
     rekog_labels = recognize_image_labels(bucket, key)
-    return image_description(rekog_labels)
+    image_desc = image_description(rekog_labels)
+    print(image_desc)
+
+    ## Pollyでテキストから音声を合成する
+    speech = text_to_speech(image_desc)
+    return 'Content-Type:{},Characters:{}'.format(speech['ContentType'], speech['RequestCharacters'])
 
 def get_s3_object(bucket, key):
     try:
@@ -50,3 +56,19 @@ def recognize_image_labels(bucket, key):
 def image_description(labels):
     top3_labels = [label['Name'] for label in labels['Labels'][:3]]
     return 'This image shows {}.'.format(', '.join(top3_labels))
+
+def text_to_speech(text):
+    try:
+        speech = polly.synthesize_speech(
+            OutputFormat='mp3',
+            SampleRate='22050',
+            Text=text,
+            TextType='text',
+            VoiceId='Joanna'  # とりあえず英語想定
+        )
+        return speech
+    except Exception as e:
+        print(e)
+        print('Error synthesizing to speech: "{}"'.format(text))
+        raise e
+
