52a735e3b4d3631ef6b9c3ae20b5db2c1aa80089
Tsunenobu Kai <kai2nenobu@gmail.com>
2017-07-22 13:35:39 +0900

    s3オブジェクトを取得する処理を関数に切り出す

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,17 +6,21 @@ import boto3
 print('Loading function')
 
 s3 = boto3.client('s3')
+rekognition = boto3.client('rekognition')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
+    s3obj = get_s3_object(event)
+    print(s3obj)
+    return s3obj['ContentType']
 
+def get_s3_object(event):
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        return response
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
